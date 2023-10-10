3442e98c7511b7ce3475ec665078c451b3023a7d
rebeccamccabe <chessgirl2400@gmail.com>
2023-10-10 19:49:10 -0400

    fix jacobs code for RM3 body creation

--- InnerOptimizer.py
+++ InnerOptimizer.py
@@ -1,44 +1,16 @@
-from datetime import datetime
 import autograd.numpy as np
 import capytaine as cpy
 import matplotlib.pyplot as plt
 from scipy.optimize import brute
 import wecopttool as wot
 
-Mensity = 5
-z = np.linspace(-4,-5.2,Mensity)
-x = 35*(z+4)/6 + 10
-y = 35*(z+4)/6 +10
-xyz = np.array([np.array([x,y,z]) for x,y,z in zip(x,y,z)])
-Frustum = cpy.FloatingBody(cpy.AxialSymmetricMesh.from_profile(xyz, nphi=Mensity**2))
+def body_from_profile(x,y,z,nphi):
+    xyz = np.array([np.array([x,y,z]) for x,y,z in zip(x,y,z)])
+    body = cpy.FloatingBody(cpy.AxialSymmetricMesh.from_profile(xyz, nphi=nphi))
+    return body
 
-w = np.linspace(0,-5.2,Mensity)
-u = 3-w+w
-v = 3-w+w
-uvw = np.array([np.array([u,v,w]) for u,v,w in zip(u,v,w)])
-Arr = cpy.FloatingBody(cpy.AxialSymmetricMesh.from_profile(uvw, nphi=Mensity**2))
-Arr+=Frustum
+def inner_function(f_max, p_max, v_max, fb, wavefreq, amplitude):
 
-t = np.linspace(0,-4,1+int(Mensity/2))
-r = 10-t+t
-s = 10-t+t
-rst = np.array([np.array([r,s,t]) for r,s,t in zip(r,s,t)])
-ArrEm = cpy.FloatingBody(cpy.AxialSymmetricMesh.from_profile(rst, nphi=Mensity**2))
-ArrEm+= Arr
-
-q = np.linspace(0,0,Mensity)
-o = 3-35*w/26
-p = 3-35*w/26
-opq = np.array([np.array([o,p,q]) for o,p,q in zip(o,p,q)])
-ArrEmThree = cpy.FloatingBody(cpy.AxialSymmetricMesh.from_profile(opq, nphi=Mensity**2))
-ArrEmThree+=ArrEm
-
-def inner_function(f_max = 2000.0, p_max = 100.0, v_max = 10000.0, mesh = rm3,
-    wavefreq = 0.3, amplitude = 1):
-    if mesh == rm3:
-        fb = ArrEmThree
-    else:
-        fb = cpy.FloatingBody.from_meshio(mesh, name="WaveBot")
     fb.add_translation_dof(name="Heave")
     ndof = fb.nb_dofs
     stiffness = wot.hydrostatics.stiffness_matrix(fb).values
@@ -104,4 +76,49 @@ def inner_function(f_max = 2000.0, p_max = 100.0, v_max = 10000.0, mesh = rm3,
         scale_obj=scale_obj,
         )
     return results.fun
-    
+
+def make_RM3():
+    h_f = 4.0           # height of straight section of float, before the frustum
+    h_f_2 = 5.2         # height of entire float, including the frustum at the bottom
+    D_s = 6.0           # diameter of spar (inner diameter of float)
+    D_f = 20.0          # outer diameter of float
+    mesh_density = 5
+
+    z1 = np.linspace(-h_f,-h_f_2,mesh_density)
+    x1 = np.linspace(D_f/2, D_s/2,mesh_density)
+    y1 = np.linspace(D_f/2, D_s/2,mesh_density)
+    bottom_frustum = body_from_profile(x1,y1,z1,mesh_density**2)
+
+    z2 = np.linspace(0,-h_f_2,mesh_density)
+    x2 = np.full_like(z2, D_s/2)
+    y2 = np.full_like(z2, D_s/2)
+    inner_surface = body_from_profile(x2,y2,z2,mesh_density**2)
+
+    z3 = np.linspace(0,-h_f,1+int(mesh_density/2))
+    x3 = np.full_like(z3, D_f/2)
+    y3 = np.full_like(z3, D_f/2)
+    outer_surface = body_from_profile(x3,y3,z3,mesh_density**2)
+    # the radius is 15 instead of 10 for some reason!?
+
+    z4 = np.linspace(0,0,mesh_density)
+    x4 = np.linspace(D_s/2, D_f/2, mesh_density)
+    y4 = np.linspace(D_s/2, D_f/2, mesh_density)
+    top_surface = body_from_profile(x4,y4,z4, mesh_density**2)
+
+    RM3 = bottom_frustum + inner_surface + outer_surface + top_surface
+
+    print('RM3 created')
+    #frustum.show_matplotlib()
+    #inner_surface.show_matplotlib()
+    #outer_surface.show_matplotlib()
+    #top_surface.show_matplotlib()
+    RM3.show_matplotlib()
+
+    return RM3
+
+if __name__ == '__main__':
+    RM3 = make_RM3()
+    #wavebot = cpy.FloatingBody.from_meshio(RM3, name="WaveBot")
+
+    #inner_function(f_max = 2000.0, p_max = 100.0, v_max = 10000.0, fb=RM3, wavefreq = 0.3, amplitude = 1)
+
