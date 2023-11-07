173434a9178b938ce773f9d070d3ee552fb1178d
En Lo <hl752@cornell.edu>
2023-11-07 11:10:28 -0500

    normalized hydro coef

--- InnerOptimizer.py
+++ InnerOptimizer.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 from scipy.optimize import brute
 import wecopttool as wot
 import math
+from capytaine.meshes.quality import (merge_duplicates, heal_normals, remove_unused_vertices,heal_triangles, remove_degenerated_faces)
+
 
 def body_from_profile(x,y,z,nphi):
-    xyz = np.array([np.array([x,y,z]) for x,y,z in zip(x,y,z)])
+    xyz = np.array([np.array([x/math.sqrt(2),y/math.sqrt(2),z]) for x,y,z in zip(x,y,z)])    # /sqrt(2) to account for the scaling
     body = cpy.FloatingBody(cpy.AxialSymmetricMesh.from_profile(xyz, nphi=nphi))
     return body
 
@@ -14,20 +16,26 @@ def inner_function(f_max, p_max, v_max, fb, wavefreq, amplitude):
 
     fb.add_translation_dof(name="Heave")
     ndof = fb.nb_dofs
+    
     stiffness = wot.hydrostatics.stiffness_matrix(fb).values
-    mass = wot.hydrostatics.inertia_matrix(fb).values
-    f1 = 0.05
+    mass = 208000
+
+    f1 = 0.02  #[Hz] 
     nfreq = 50
     freq = wot.frequency(f1, nfreq, False) # False -> no zero frequency
     bem_data = wot.run_bem(fb, freq)
+    
     name = ["PTO_Heave",]
     kinematics = np.eye(ndof)
     controller = None
     loss = None
     pto_impedance = None
     pto = wot.pto.PTO(ndof, kinematics, controller, pto_impedance, loss, name)
+    
     f_add = {'PTO': pto.force_on_wec}
+    
     nsubsteps = 4
+    
     def const_f_pto(wec, x_wec, x_opt, waves): # Format for scipy.optimize.minimize
         f = pto.force_on_wec(wec, x_wec, x_opt, waves, nsubsteps)
         return f_max - np.abs(f.flatten())
@@ -37,6 +45,7 @@ def inner_function(f_max, p_max, v_max, fb, wavefreq, amplitude):
     def const_v_pto(wec, x_wec, x_opt, waves): # Format for scipy.optimize.minimize
         v = pto.velocity(wec, x_wec, x_opt, waves, nsubsteps)
         return v_max - np.abs(v.flatten())
+
     ineq_cons1 = {'type': 'ineq',
                  'fun': const_f_pto,
                  }
@@ -47,13 +56,16 @@ def inner_function(f_max, p_max, v_max, fb, wavefreq, amplitude):
                  'fun': const_v_pto,
                  }
     constraints = [ineq_cons1,ineq_cons2,ineq_cons3]
+    
     wec = wot.WEC.from_bem(
         bem_data,
         inertia_matrix=mass,
         hydrostatic_stiffness=stiffness,
         constraints=constraints,
         friction=None,
-        f_add=f_add,)
+        f_add=f_add,
+    )
+    
     phase = 30
     wavedir = 0
     waves = wot.waves.regular_wave(f1, nfreq, wavefreq, amplitude, phase, wavedir)
@@ -67,6 +79,7 @@ def inner_function(f_max, p_max, v_max, fb, wavefreq, amplitude):
     scale_x_wec = 1e1
     scale_x_opt = 1e-3
     scale_obj = 1e-2
+    
     results = wec.solve(
         waves,
         obj_fun,
@@ -78,9 +91,13 @@ def inner_function(f_max, p_max, v_max, fb, wavefreq, amplitude):
         )
     
     # graph for hyrdodynamics coefficients
+    rho=1030
+    Added_mass_norm = bem_data['added_mass']/rho
+    radiation_dampin_normg = bem_data['radiation_damping']/(rho*f1*2*math.pi)
+    
     fig, axes = plt.subplots(3,1)
-    bem_data['added_mass'].plot(ax = axes[0])
-    bem_data['radiation_damping'].plot(ax = axes[1])
+    Added_mass_norm.plot(ax = axes[0])
+    radiation_dampin_normg.plot(ax = axes[1])
     axes[2].plot(bem_data['omega'],np.abs(np.squeeze(bem_data['diffraction_force'].values)), color = 'orange')
     axes[2].set_ylabel('abs(diffraction_force)', color = 'orange')
     axes[2].tick_params(axis ='y', labelcolor = 'orange')
@@ -105,34 +122,31 @@ def make_RM3():
     D_f = 20.0          # outer diameter of float
     mesh_density = 5
 
-    z1 = np.linspace(-h_f,-h_f_2,mesh_density)
-    x1 = np.linspace(D_f/2, D_s/2, mesh_density)    # /1,414 to account for the scaling of sqrt(2) 
-    y1 = np.linspace(D_f/2, D_s/2,mesh_density)
-    bottom_frustum = body_from_profile(x1/math.sqrt(2),y1/math.sqrt(2),z1,mesh_density**2)
+    #normal vectors have to be facing outwards
+    z1 = np.linspace(-h_f_2,-h_f,mesh_density)
+    x1 = np.linspace(D_s/2, D_f/2, mesh_density) 
+    y1 = np.linspace(D_s/2, D_f/2,mesh_density)
+    bottom_frustum = body_from_profile(x1,y1,z1,mesh_density**2)
 
-    z2 = np.linspace(0,-h_f_2,mesh_density)
+    z2 = np.linspace(0, -h_f_2, mesh_density)
     x2 = np.full_like(z2, D_s/2)
     y2 = np.full_like(z2, D_s/2)
-    inner_surface = body_from_profile(x2/math.sqrt(2),y2/math.sqrt(2),z2,mesh_density**2)
+    inner_surface = body_from_profile(x2,y2,z2,mesh_density**2)
 
-    z3 = np.linspace(0,-h_f,1+int(mesh_density/2))
+    z3 = np.linspace(-h_f, 0, 1+int(mesh_density/2))
     x3 = np.full_like(z3, D_f/2)
     y3 = np.full_like(z3, D_f/2)
-    outer_surface = body_from_profile(x3/math.sqrt(2),y3/math.sqrt(2),z3,mesh_density**2)
-    # the radius is 15 instead of 10 for some reason!?
+    outer_surface = body_from_profile(x3,y3,z3,mesh_density**2)
 
     z4 = np.linspace(0,0,mesh_density)
-    x4 = np.linspace(D_s/2, D_f/2, mesh_density)
-    y4 = np.linspace(D_s/2, D_f/2, mesh_density)
-    top_surface = body_from_profile(x4/math.sqrt(2),y4/math.sqrt(2),z4, mesh_density**2)
+    x4 = np.linspace(D_f/2, D_s/2, mesh_density)
+    y4 = np.linspace(D_f/2, D_s/2, mesh_density)
+    top_surface = body_from_profile(x4,y4,z4, mesh_density**2)
 
-    RM3 = bottom_frustum + inner_surface + outer_surface + top_surface
+    RM3 = bottom_frustum + outer_surface + top_surface + inner_surface
 
+    
     print('RM3 created')
-    #frustum.show_matplotlib()
-    #inner_surface.show_matplotlib()
-    #outer_surface.show_matplotlib()
-    #top_surface.show_matplotlib()
     RM3.show_matplotlib()
 
     return RM3
@@ -142,3 +156,4 @@ if __name__ == '__main__':
     #wavebot = cpy.FloatingBody.from_meshio(RM3, name="WaveBot")
 
     #inner_function(f_max = 2000.0, p_max = 100.0, v_max = 10000.0, fb=RM3, wavefreq = 0.3, amplitude = 1)
+    
