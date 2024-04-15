8e8f4f9d9d62ee2381644ac41489167c75b22ea4
leahbuccino <142751599+leahbuccino@users.noreply.github.com>
2024-04-15 16:35:31 -0400

    Update environmentComponent.py

--- openMDAO/environmentComponent.py
+++ openMDAO/environmentComponent.py
@@ -1,32 +1,44 @@
-import openmdao.api as om
-import numpy as np
-
 class environmentComponent(om.ExplicitComponent):
     def setup(self):
 
         #adding inputs:
         some_float_value = 0.0
-        self.add_input(name = 'firstFloatInput', val = 0.0, desc="this is my first float variable")
+        self.add_input(name = 'steel', val = 0.0, desc="amoutn of steel (kg)")
+        self.add_input(name = 'distance', val = 0.0, desc="distance from shore (miles)")
+        self.add_input(name = 'fiberglass', val = 0.0, desc="amount of fiberglass (m^2)")
+        self.add_input(name = 's_points', val = 0.17640291442619996, desc="steel eco cost (euro/kg)")
+        self.add_input(name = 'f_points', val = 6.375740909087548, desc="fiberglass eco cost (euro/m^2)")
+        self.add_input(name = 'd_points', val = 60.44128387234, desc="travel eco cost (euro/mi)")
+        self.add_input(name = 'CEM_output', val = 0.0, desc="avoided carbon (Mtons CO2)")
+        self.add_input(name = 'SCC', val = 0.133, desc="social cost of carbon (euros/kg CO2)")
         self.add_input(name = 'firstFloatMatrixInput', val = np.zeros((4,5)), shape = (4,5), desc="this is my first float matrix variable" )
 
         #adding output:
-        self.add_output(name = 'firstFloatOutput', desc = "this is my first float variable output")
+        self.add_output(name = 'eco_value', desc = "total eco value")
         self.add_output(name = 'firstFloatMatrixOutput', shape=(4,5), desc = "this is my first float matrix variable output")
 
         # Partial derivatives required for optimization
         self.declare_partials('*', '*', method='fd')
 
-
     def compute(self, inputs, outputs):
         #retrieve inputs
-        firstFloatInput = inputs['firstFloatInput'][0] #[0] index, retrieve float number, otherwise it is an array
+        CEM_output = inputs['CEM_output'][0] #[0] index, retrieve float number, otherwise it is an array
+        SCC = inputs['SCC'][0] #[0] index, retrieve float number, otherwise it is an array
+        s_points = inputs['s_points'][0] #[0] index, retrieve float number, otherwise it is an array
+        steel = inputs['steel'][0] #[0] index, retrieve float number, otherwise it is an array
+        f_points = inputs['f_points'][0]
+        fiberglass = inputs['fiberglass'][0]
+        d_points = inputs['d_points'][0]
+        distance = inputs['distance'][0]
         firstFloatMatrixInput = inputs['firstFloatMatrixInput']
 
-        firstFloatOutput = 2 * firstFloatInput
+        CEM_points = CEM_output * SCC
+
+        eco_value =  -(s_points * steel + f_points * fiberglass + d_points * distance) + CEM_points
         firstFloatMatrixOutput = firstFloatOutput * firstFloatMatrixInput
 
         #assign outputs
-        outputs['firstFloatOutput'] = firstFloatOutput
+        outputs['eco_value'] = eco_value
         outputs['firstFloatMatrixOutput'] = firstFloatMatrixOutput
 
 
@@ -38,7 +50,15 @@ prob.setup()
 firstFloatInput = 0.2
 firstFloatMatrixInput = np.ones((4,5))
 
-prob.set_val('test.firstFloatInput', firstFloatInput)
+prob.set_val('test.steel', firstFloatInput)
+prob.set_val('test.distance', firstFloatInput)
+prob.set_val('test.fiberglass', firstFloatInput)
+prob.set_val('test.s_points', firstFloatInput)
+prob.set_val('test.f_points', firstFloatInput)
+prob.set_val('test.d_points', firstFloatInput)
+prob.set_val('test.CEM_output', firstFloatInput)
+prob.set_val('test.SCC', firstFloatInput)
+
 prob.set_val('test.firstFloatMatrixInput', firstFloatMatrixInput)
 
 prob.run_model()
