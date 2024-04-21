6e01623429978399de066e27e93024e31dc3ea6d
Rebecca McCabe <chessgirl2400@gmail.com>
2024-04-21 00:16:13 -0400

    add conditional logic to only download data if absent

--- modules/CEM/get-powergenome-data.sh
+++ modules/CEM/get-powergenome-data.sh
@@ -1,21 +1,31 @@
 # download zip files and unzip them
 # once powergenome data sources are hosted on zenodo instead of drive, use pooch for this instead of gdown
-gdown -O data/cambium.zip 1nbhWwOsNeOtcUew9Mn4QGuAtCsZo0VZ2
-unzip -n ./data/cambium.zip -d ./data/cambium
+if ! [ -d ./data/cambium ]; then
+    gdown -O data/cambium.zip 1nbhWwOsNeOtcUew9Mn4QGuAtCsZo0VZ2
+    unzip -n ./data/cambium.zip -d ./data/cambium
+fi
 
-gdown -O data/efs.zip 1dWA35bQpPksnSb6auybMbrIqyaBG6wBM
-unzip -n ./data/efs.zip -d ./data/efs
+if ! [ -d ./data/efs ]; then
+    gdown -O data/efs.zip 1dWA35bQpPksnSb6auybMbrIqyaBG6wBM
+    unzip -n ./data/efs.zip -d ./data/efs
+fi
 
-gdown -O data/pg.zip 1XrLOqVGNP1qjvsXeTt1YH2Pyppqad0fc
-unzip -n ./data/pg.zip -d ./data/pg
+if ! [ -d ./data/pg ]; then
+    gdown -O data/pg.zip 1XrLOqVGNP1qjvsXeTt1YH2Pyppqad0fc
+    unzip -n ./data/pg.zip -d ./data/pg
+fi
 
-gdown -O data/pudl.zip 1tJipxJYxP_dcAnopJrdXdcZh7K3SlI1-
-unzip -n ./data/pudl.zip -d ./data/pudl
+if ! [ -d ./data/pudl ]; then
+    gdown -O data/pudl.zip 1tJipxJYxP_dcAnopJrdXdcZh7K3SlI1-
+    unzip -n ./data/pudl.zip -d ./data/pudl
+fi
 
 # delete zips after extraction
-rm ./data/*.zip
+rm -f ./data/*.zip
 
-gdown --folder -O data/resource_profiles 1ZYxnl4U_3HXlYPxm8qlmqyWB8NyC3PpG # this one isn't a zip
+if ! [ -d ./data/resource_profiles ]; then
+    gdown --folder -O data/resource_profiles 1ZYxnl4U_3HXlYPxm8qlmqyWB8NyC3PpG # this one isn't a zip
+fi
 
 # run sweep
-run_powergenome_multiple -sf ../GenX/Example_Systems/RealSystemExample/ISONE_Singlezone/settings/sweeps_settings.yml -rf ./modules/CEM/results
+run_powergenome_multiple -sf ./modules/CEM/sweeps_settings.yml -rf ./modules/CEM/results
