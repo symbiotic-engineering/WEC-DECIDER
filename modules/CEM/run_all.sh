f21a7646f6931fc16b98b8b2a49304f88f2657b0
Rebecca McCabe <chessgirl2400@gmail.com>
2025-07-12 01:19:39 -0400

    fix basic syntax of run all script

--- modules/CEM/run_all.sh
+++ modules/CEM/run_all.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+set -e # if any command fails, exit immediately. Comment this out if you are using a debugger and have breakpoints on errors.
 
-conda init
-conda activate wec-decider-decider # a clone of wec-decider-7-pg-edit but with calkit/nbconvert installed
+function run_notebook() {
+    local folder="$1"
+    local notebook_path="$2"
+    jupyter nbconvert --execute "$folder/$notebook_path" --to notebook --output "$notebook_path"
+}
 
-run_notebook("modules/CEM/make_additional_tech_csv.ipynb")
+# to avoid conda error, run this script as "bash -i modules/CEM/run_all.sh"
+conda activate wec-decider-decider-2 # a clone of wec-decider-7-pg-edit but with calkit/nbconvert installed
+
+run_notebook modules/CEM make_additional_tech_csv.ipynb
 
 bash modules/CEM/get-powergenome-data.sh
 bash modules/CEM/run_powergenome.sh
@@ -11,9 +18,4 @@ bash modules/CEM/run_powergenome.sh
 #julia modules/CEM/setup.jl
 julia modules/CEM/Run.jl
 
-run_notebook("modules/CEM/analyze_results.ipynb")
-
-function run_notebook() {
-    local notebook_path="$1"
-    jupyter nbconvert --execute "$notebook_path" --to notebook --output "$notebook_path"
-}
+run_notebook modules/CEM analyze_results.ipynb
