82ab87597f1e0f6fb347ae383a22ac3a1210ea38
Rebecca McCabe <chessgirl2400@gmail.com>
2025-07-16 01:09:08 -0400

    add echos with timestamp to run all script

--- modules/CEM/run_all.sh
+++ modules/CEM/run_all.sh
@@ -7,15 +7,21 @@ function run_notebook() {
     jupyter nbconvert --execute "$folder/$notebook_path" --to notebook --output "$notebook_path"
 }
 
+echo "$(date +"%Y-%m-%d %H:%M:%S") Activating conda environment"
 # to avoid conda error, run this script as "bash -i modules/CEM/run_all.sh"
 conda activate wec-decider-decider-2 # a clone of wec-decider-7-pg-edit but with calkit/nbconvert installed
 
+echo "$(date +"%Y-%m-%d %H:%M:%S") Creating sweep inputs"
 run_notebook modules/CEM make_additional_tech_csv.ipynb
 
+echo "$(date +"%Y-%m-%d %H:%M:%S") Downloading PowerGenome data"
 bash modules/CEM/get-powergenome-data.sh
+echo "$(date +"%Y-%m-%d %H:%M:%S") Running PowerGenome"
 bash modules/CEM/run_powergenome.sh
 
+echo "$(date +"%Y-%m-%d %H:%M:%S") Running GenX"
 #julia modules/CEM/setup.jl
 julia modules/CEM/Run.jl
 
+echo "$(date +"%Y-%m-%d %H:%M:%S") Plotting results"
 run_notebook modules/CEM analyze_results.ipynb
