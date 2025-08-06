63ad23b7240acb4e588bb4cf08d37643315be6d9
Rebecca McCabe <chessgirl2400@gmail.com>
2025-08-06 00:29:02 -0400

    fix double settings folder in powergenome bash

--- modules/CEM/run_powergenome.sh
+++ modules/CEM/run_powergenome.sh
@@ -13,7 +13,7 @@ function run_for_one_location {
     run_powergenome_multiple -sf "$script_dir/$location/settings/" -rf "$script_dir/$location/cases"
     for i in "$script_dir/$location/cases"/Case_*; do
         mkdir -p "$i/settings" # -p creates directory only if it does not already exist
-        cp -r "$script_dir/template/settings/" "$i/settings/."
+        cp -r "$script_dir/template/settings/" "$i/."
     done
 
     echo "PowerGenome ran for $location."
