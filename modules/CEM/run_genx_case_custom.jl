590b244fdc6f18532998fb2191d4158974143eed
Kalvindra Parasram <kp579@cornell.edu>
2025-12-11 05:05:25 -0500

    cleaned up run_genx_case_custom.jl

--- modules/CEM/run_genx_case_custom.jl
+++ modules/CEM/run_genx_case_custom.jl
@@ -20,81 +20,6 @@ using GenX:
 
 export run_genx_case_simple!
 
-# function run_genx_case_simple!(case::AbstractString, mysetup::Dict, optimizer::Any)
-#     settings_path = get_settings_path(case)
-
-#     ### Cluster time series inputs if necessary and if specified by the user
-#     if mysetup["TimeDomainReduction"] == 1
-#         TDRpath = joinpath(case, mysetup["TimeDomainReductionFolder"])
-#         system_path = joinpath(case, mysetup["SystemFolder"])
-#         prevent_doubled_timedomainreduction(system_path)
-#         if !time_domain_reduced_files_exist(TDRpath)
-#             println("Clustering Time Series Data (Grouped)...")
-#             cluster_inputs(case, settings_path, mysetup)
-#         else
-#             println("Time Series Data Already Clustered.")
-#         end
-#     end
-
-#     ### Configure solver
-#     println("Configuring Solver")
-#     solver_name = lowercase(get(mysetup, "Solver", ""))
-#     OPTIMIZER = configure_solver(settings_path, optimizer; solver_name=solver_name)
-
-#     #### Running a case
-
-#     ### Load inputs
-#     println("Loading Inputs")
-#     myinputs = load_inputs(mysetup, case)
-
-#     println("Generating the Optimization Model")
-#     time_elapsed = @elapsed EP = generate_model(mysetup, myinputs, OPTIMIZER)
-#     println("Time elapsed for model building is")
-#     println(time_elapsed)
-
-#     println("Solving Model")
-#     EP, solve_time = solve_model(EP, mysetup)
-#     myinputs["solve_time"] = solve_time # Store the model solve time in myinputs
-
-#     # Run MGA if the MGA flag is set to 1 else only save the least cost solution
-#     if has_values(EP)
-#         println("Writing Output")
-#         outputs_path = get_default_output_folder(case)
-#         elapsed_time = @elapsed outputs_path = write_outputs(EP,
-#             outputs_path,
-#             mysetup,
-#             myinputs)
-#         println("Time elapsed for writing is")
-#         println(elapsed_time)
-#         if mysetup["ModelingToGenerateAlternatives"] == 1
-#             println("Starting Model to Generate Alternatives (MGA) Iterations")
-#             mga(EP, case, mysetup, myinputs)
-#         end
-
-#         if mysetup["MethodofMorris"] == 1
-#             println("Starting Global sensitivity analysis with Method of Morris")
-#             morris(EP, case, mysetup, myinputs, outputs_path, OPTIMIZER)
-#         end
-#     end
-# end
-
-# Helper function to safely extract a dual value
-function get_dual(con, g)
-    if con === nothing
-        return NaN
-    end
-
-    # DenseAxisArray indexing (the common GenX form)
-    inds = con.axes[1]
-
-    if g in inds
-        return dual(con[g])
-    else
-        return NaN
-    end
-end
-
-
 function run_genx_case_simple!(case::AbstractString, mysetup::Dict, optimizer::Any)
     println("Case directory = $case")
 
@@ -133,13 +58,8 @@ function run_genx_case_simple!(case::AbstractString, mysetup::Dict, optimizer::A
     end
 
    # Commenting these out to keep multistage active
-   # mysetup["MultiStage"] = 0
-   # mysetup["MultiStageSettingsDict"] = Dict()
-
-
-    # if !haskey(mysetup, "OPEXMULT")
-    #     mysetup["OPEXMULT"] = 1.0
-    # end
+   mysetup["MultiStage"] = 0
+   mysetup["MultiStageSettingsDict"] = Dict()
 
     ### Cluster time series inputs if necessary
     if mysetup["TimeDomainReduction"] == 1
@@ -185,37 +105,6 @@ function run_genx_case_simple!(case::AbstractString, mysetup::Dict, optimizer::A
     EP, solve_time = solve_model(EP, mysetup)
     myinputs["solve_time"] = solve_time
 
-    println(typeof(myinputs["G"]))
-    println(typeof(myinputs["RESOURCES"]))
-
-    #print the variables in EP
-    open(joinpath(case, "EP_fields.txt"), "w") do io
-        println(io, "=== Constraints in EP ===")
-        for c in all_constraints(EP; include_variable_in_set_constraints=false)
-            cname = JuMP.name(c) === nothing ? "<unnamed>" : JuMP.name(c)
-            println(io, cname, " :: ", typeof(c))
-        end
-    end
-
-
-    println("EP field names written to EP_fields.txt")
-
-    # #for printing out each variable in EP and their types
-    # open(joinpath(case, "EP_field_types.txt"), "w") do io
-    #     println(io, "=== Fields and their types ===")
-    #     for fn in fieldnames(typeof(EP))
-    #         try
-    #             val = getfield(EP, fn)
-    #             println(io, fn, " => ", typeof(val))
-    #         catch
-    #             println(io, fn, " => FAILED TO READ")
-    #         end
-    #     end
-    # end
-
-    # println("EP field types written to EP_field_types.txt")
-
-
     ### Print all dual constrains in EP object
     output_file = joinpath(case, "EP_duals.txt")
     open(output_file, "w") do io
@@ -258,67 +147,6 @@ function run_genx_case_simple!(case::AbstractString, mysetup::Dict, optimizer::A
             end
         end
     end
-
-    #     println("Extracting power max duals...")
-    # powermax = extract_power_max_duals(EP)
-
-    # println("Extracting power min duals...")
-    # powermin = extract_power_min_duals(EP)
-
-    # #Convert to DataFrames
-    # df_max = DataFrame(powermax)
-    # df_min = DataFrame(powermin)
-
-    # rename!(df_max, :dual => :powermax_dual)
-    # rename!(df_min, :dual => :powermin_dual)
-
-    # # Sort & merge
-    # sort!(df_max, [:g, :t])
-    # sort!(df_min, [:g, :t])
-
-    # df = innerjoin(df_max, df_min, on = [:g, :t])
-
-    # # ---------------------------------------------------------
-    # # Save to CSV
-    # # ---------------------------------------------------------
-    # output_csv = joinpath(case, "Power_Duals.csv")
-    # CSV.write(output_csv, df)
-
-    # println("Wrote merged power duals to: ", output_csv)
-
-    # === Build dual table ===
-    # gen_list = myinputs["RESOURCES"]
-    # num_gens = length(gen_list)
-
-    # df = DataFrame(
-    #     Index = Int[],
-    #     Name = String[],
-    #     cMaxCap = Float64[],
-    #     cMinCapEnergyDuration = Float64[]
-    # )
-
-    # maxcap_con = haskey(EP, :cMaxCap) ? EP[:cMaxCap] : nothing
-    # mindur_con = haskey(EP, :cMinCapEnergyDuration) ? EP[:cMinCapEnergyDuration] : nothing
-
-    # for g in 1:num_gens
-    #     res = gen_list[g]
-
-    #     push!(df, (
-    #         g,
-    #         string(res.id),   # works for all thermals/VRE/hydro/storage
-
-    #         get_dual(maxcap_con, g),
-    #         get_dual(mindur_con, g)
-    #     ))
-    # end
-
-
-    # # Write CSV
-    # output_file = joinpath(case, "dual_table.csv")
-    # CSV.write(output_file, df)
-
-    # println("Dual table written to $output_file")
-
 
     ### Write outputs and optionally run MGA / Morris
     if has_values(EP)
